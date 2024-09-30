664c1892bd68af013e47e92e061f0d8830da5a5b
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-30 19:06:57 -0400

    stars

--- A/sketch.js
+++ A/sketch.js
@@ -1,18 +1,13 @@
-function star(x, y, w, c) {
+function star(x, y, w, c, l) {
   let r = w / 2;
   push();
   translate(x, y);
-  line(c, -c, r, 0);
-  line(c, c, r, 0);
-
-  line(-c, c, 0, r);
-  line(c, c, 0, r);
-
-  line(-c, -c, -r, 0);
-  line(-c, c, -r, 0);
-
-  line(-c, -c, 0, -r);
-  line(c, -c, 0, -r);
+  for (let cnt = 0; cnt < l; cnt++) {
+    print(cnt);
+    line(c, -c, r, 0);
+    line(c, c, r, 0);
+    rotate(360 / l);
+  }
   pop();
 }
 
@@ -20,10 +15,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(150, 150, 220);
   strokeWeight(2);
+  angleMode(DEGREES);
 
-  star(width / 4, height / 2, 200, 10);
-  star(width / 2, height / 2, 350, 25);
-  star(3 * width / 4, height / 2, 100, 10);
+  star(width / 4, height / 2, 200, 10, 12);
+  star(width / 2, height / 2, 350, 25, 200);
+  star((3 * width) / 4, height / 2, 100, 10, 6);
 }
 
 function draw() {}

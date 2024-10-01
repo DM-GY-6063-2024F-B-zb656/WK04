05f4b167defcd25b24ce66fb9db45c3abba8fc9b
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-30 20:56:37 -0400

    randomness part 2

--- D/sketch.js
+++ D/sketch.js
@@ -1,3 +1,16 @@
+function star(x, y, w, c, l) {
+  let r = w / 2;
+  push();
+  translate(x, y);
+  for (let cnt = 0; cnt < l; cnt++) {
+    print(cnt);
+    line(c, -c, r, 0);
+    line(c, c, r, 0);
+    rotate(360 / l);
+  }
+  pop();
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
@@ -9,19 +22,44 @@ function setup() {
   let sqDim = 0.9 * pitch;
   translate(pitch / 2, pitch / 2);
 
-
   // randomSeed(1010);
-  //this starts the random at the same point every time, so the things generated are random but will stay the same on every refresh
   // draw a column of rows
   for (let y = 0; y < height; y += pitch) {
     // draw a row of squares
     for (let x = 0; x < width; x += pitch) {
+      let rn = random(0, 6);
+      let rr = random(256);
+      let rg = random(256);
+      let rb = random(256);
+
+      let rcc = random(["#f5b000", "#008a0c", "#0e52e8"]); //< use sq brackets for an array
+
       push();
       translate(x, y);
-      rect(0, 0, sqDim);
+
+      //conditionalsvv
+      noFill();
+      strokeWeight(5);
+      stroke(rcc);
+      if (rn < 2) {
+        rect(0, 0, sqDim);
+      } else if (rn < 4) {
+        ellipse(0, 0, sqDim);
+      } else {
+        star(0, 0, sqDim, 10, 4);
+      }
+
       pop();
     }
   }
 }
 
 function draw() {}
+
+// if (whatevers in these parentheses is true) {
+//   then do tis thing;
+// } else if (whatevers in these parenthesis is true) {
+// then do this thing;
+// } else {
+// do this thing
+// }
